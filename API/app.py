d6f5faa131521605f0eee0b46df3bd254617424b
taha <m.taha@technologyally.com>
2019-11-27 21:40:45 +0500

    work for model

--- API/app.py
+++ API/app.py
@@ -5,6 +5,22 @@ import os
 
 #init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Databse
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init DB
+db = SQLAlchemy(app)
+# init marshmallow
+ma = Marshmallow(app)
+
+# Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
 
 # Run Server
 if __name__ == '__main__':
